5cd9dd316a5955b72c54158411897f86b6a2c5c0
Renan Guedes <renan.q.guedes@gmail.com>
2021-11-11 16:37:42 -0300

    Alteração no layout do portfólio e atualizações de projetos

--- rocketseat-beauty-saloon/assets/js/main.js
+++ rocketseat-beauty-saloon/assets/js/main.js
@@ -66,8 +66,8 @@ scrollReveal.reveal(
   { interval: 50 }
 )
 
-/* back to top button */
-window.onload = function() {
+/*****  back to top button *****/
+window.onload = function () {
   const backToTopButton = document.querySelector('.back-to-top')
   const footer = document.querySelector('#footer')
   const footerHeigth = footer.offsetHeight
@@ -75,60 +75,60 @@ window.onload = function() {
   const topFooter = windowHeight - footerHeigth - window.innerHeight
   function backToTop() {
     if (window.scrollY >= 480) {
-    backToTopButton.classList.add('show')
+      backToTopButton.classList.add('show')
     } else {
-    backToTopButton.classList.remove('show')
+      backToTopButton.classList.remove('show')
     }
   }
-/* change color back to top button when on footer */
-  console.log("meu calculo foi",windowHeight)
+  /* change color back to top button when on footer */
+  console.log("meu calculo foi", windowHeight)
   function changeColorBackToTopButton() {
     if (window.scrollY >= topFooter) {
-    backToTopButton.classList.add('add')
+      backToTopButton.classList.add('add')
     } else {
-    backToTopButton.classList.remove('add')
+      backToTopButton.classList.remove('add')
     }
   }
 
-/* Activate menu alt color at current section */
-const sections = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection() {
+  /* Activate menu alt color at current section */
+  const sections = document.querySelectorAll('main section[id]')
+  function activateMenuAtCurrentSection() {
 
-  const sectionLimit = window.pageYOffset + (window.innerHeight / 8) * 3 /* creat a 'imaginare'limit line on viewprt */
+    const sectionLimit = window.pageYOffset + (window.innerHeight / 8) * 3 /* creat a 'imaginare'limit line on viewprt */
 
-  for (const section of sections) {
-    const sectionTop = section.offsetTop /* define the top of each section  */
-    const sectionHeight = section.offsetHeight /* define the hieght of section*/
-    const sectionId =
-      section.getAttribute('id') /* get id from section  */
+    for (const section of sections) {
+      const sectionTop = section.offsetTop /* define the top of each section  */
+      const sectionHeight = section.offsetHeight /* define the hieght of section*/
+      const sectionId =
+        section.getAttribute('id') /* get id from section  */
 
-    const startSectionLimit =
-      sectionLimit >= sectionTop /* define where start section*/
-    const endSectionLimit =
-      sectionLimit <=
-      sectionTop + sectionHeight /* define where end section*/
+      const startSectionLimit =
+        sectionLimit >= sectionTop /* define where start section*/
+      const endSectionLimit =
+        sectionLimit <=
+        sectionTop + sectionHeight /* define where end section*/
 
-    if (startSectionLimit && endSectionLimit) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add(
-          'active'
-        ) /*concatenate the selectors 'nav ul li a' + id that is allocated in the variable 'sectionId' and ADD the class .active  */
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove(
-          'active'
-        ) /* concatenate the selectors 'nav ul li a' + the id that is allocated in the variable 'sectionId' and REMOVE the class .active  */
+      if (startSectionLimit && endSectionLimit) {
+        document
+          .querySelector('nav ul li a[href*=' + sectionId + ']')
+          .classList.add(
+            'active'
+          ) /*concatenate the selectors 'nav ul li a' + id that is allocated in the variable 'sectionId' and ADD the class .active  */
+      } else {
+        document
+          .querySelector('nav ul li a[href*=' + sectionId + ']')
+          .classList.remove(
+            'active'
+          ) /* concatenate the selectors 'nav ul li a' + the id that is allocated in the variable 'sectionId' and REMOVE the class .active  */
+      }
     }
   }
-}
 
-/* When Scroll */
-window.addEventListener('scroll', function () {
-  shadowHeaderWhenScroll(),
-    backToTop(),
-    changeColorBackToTopButton(),
-    activateMenuAtCurrentSection()
-})
+  /* When Scroll */
+  window.addEventListener('scroll', function () {
+    shadowHeaderWhenScroll(),
+      backToTop(),
+      changeColorBackToTopButton(),
+      activateMenuAtCurrentSection()
+  })
 }
